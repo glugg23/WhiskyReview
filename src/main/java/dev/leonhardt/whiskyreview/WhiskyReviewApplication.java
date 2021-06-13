6753fcf96cbfb389fd21fd433f847062e240a495
Max Leonhardt <m.leonhardt424@gmail.com>
2021-06-13 14:54:34 +0100

    Initial commit

---
+++ src/main/java/dev/leonhardt/whiskyreview/WhiskyReviewApplication.java
@@ -0,0 +1,11 @@
+package dev.leonhardt.whiskyreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyReviewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WhiskyReviewApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/leonhardt/whiskyreview/WhiskyReviewApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.leonhardt.whiskyreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskyReviewApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
